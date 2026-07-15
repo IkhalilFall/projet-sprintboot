e583d2109402e90f5ed26316b359524cf3e43ad2
maryahmad1003 <jesuisetudiant10@gmail.com>
2026-07-15 17:30:27 +0000

    Ajout de la configuration Swagger/OpenAPI

---
+++ src/main/java/com/ipd/Gestion_Scolaire/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.ipd.Gestion_Scolaire.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Gestion Scolaire API",
+                version = "1.0.0",
+                description = "API REST pour la gestion scolaire avec authentification JWT",
+                contact = @Contact(name = "Equipe projet")
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
