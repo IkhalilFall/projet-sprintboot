7301a7ca71f3cbc1710404117d22048fe07c743a
IkhalilFall <khalilfall52@mail.com>
2026-07-16 16:27:09 +0000

    Resolution complete des conflits Git

--- src/main/java/com/ipd/Gestion_Scolaire/entity/RefreshToken.java
+++ src/main/java/com/ipd/Gestion_Scolaire/entity/RefreshToken.java
@@ -18,17 +18,20 @@ public class RefreshToken {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(nullable = false, unique = true, length = 500)
     private String token;
+
 
     @Column(nullable = false)
     private Instant expiryDate;
 
+
     @OneToOne
-<<<<<<< HEAD
-    @JoinColumn(name = "user_id", unique = true)
-=======
-    @JoinColumn(name = "user_id", nullable = false, unique = true)
->>>>>>> origin/develop
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            unique = true
+    )
     private User user;
 }
